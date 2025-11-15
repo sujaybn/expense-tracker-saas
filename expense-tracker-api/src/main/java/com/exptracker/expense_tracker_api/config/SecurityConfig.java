8802e4a60bdc8a0ecc3c2a8f06b2bf56ae5aaa1a
sujaybn <89024189+sujaybn@users.noreply.github.com>
2025-11-15 13:20:27 +0530

    Updated security config

--- expense-tracker-api/src/main/java/com/exptracker/expense_tracker_api/config/SecurityConfig.java
+++ expense-tracker-api/src/main/java/com/exptracker/expense_tracker_api/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
